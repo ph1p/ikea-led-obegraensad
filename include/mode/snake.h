8a8707ef5efe8b7eb414f364834591b8314f6ebe
cixio <5869764+cixio@users.noreply.github.com>
2022-12-29 22:56:55 +0100

    cleanup + using loop + better game end

--- include/mode/snake.h
+++ include/mode/snake.h
@@ -7,55 +7,21 @@
 class Snake
 {
 private:
-  static const unsigned int DEBOUNCE_TIME = 100;
-  static const unsigned int X_MAX = 16;
-  static const unsigned int Y_MAX = 16;
-  static const unsigned int BRICK_AMOUNT = X_MAX * 4;
-  static const unsigned int BALL_DELAY_MAX = 250;
-  static const unsigned int BALL_DELAY_MIN = 100;
-  static const unsigned int BALL_DELAY_STEP = 5;
-  static const unsigned int PADDLE_WIDTH = 4;
-  static const unsigned int DIRECTION_NONE = 0;
-  static const unsigned int DIRECTION_LEFT = 1;
-  static const unsigned int DIRECTION_RIGHT = 2;
   static const unsigned int LED_TYPE_OFF = 0;
   static const unsigned int LED_TYPE_ON = 1;
   static const unsigned int GAME_STATE_RUNNING = 1;
   static const unsigned int GAME_STATE_END = 2;
-  static const unsigned int GAME_STATE_LEVEL = 3;
-  struct Coords
-  {
-    byte x;
-    byte y;
-  };
 
   byte gameState;
-  byte level;
-  byte destroyedBricks;
-  Coords paddle[Snake::PADDLE_WIDTH];
-  Coords bricks[Snake::BRICK_AMOUNT];
-  Coords ball;
 
-  int ballMovement[2];
-  unsigned int ballDelay;
-  unsigned int score;
   std::vector<int> position = {240,241,242};
   unsigned int dot;
-  unsigned long lastBallUpdate = 0;
 
-  void resetLEDs();
   void initGame();
   void initSnake();
   void newDot();
   void findDirection();
   void moveSnake(int newpos);
-
-
-  void newLevel();
-  void updateBall();
-  void hitBrick(byte i);
-  void checkPaddleCollision();
-  void updatePaddle();
   void end();
 
 public:
--- src/mode/snake.cpp
+++ src/mode/snake.cpp
@@ -4,14 +4,14 @@ void Snake::initGame()
 {
   Screen.clear();
 
-  //this->ballDelay = Snake::BALL_DELAY_MAX;
+  //this->ballDelay = ;
   //this->score = 0;
-  //this->level = 0;
   initSnake();
 }
 
 void Snake::initSnake()
 {
+  this->position = {240,241,242};
   for (const int &n: this->position)
   {
     Screen.setPixelAtIndex(n, Snake::LED_TYPE_ON);
@@ -35,8 +35,6 @@ void Snake::newDot() {
   Screen.render();
   
   this->gameState = Snake::GAME_STATE_RUNNING;
-
-  findDirection(); // just here for testing. uncomment this function in loop
 }
 
 void Snake::findDirection() {
@@ -108,7 +106,6 @@ void Snake::findDirection() {
     up = 0;
   }
 
-  // if everything is set to 0 the next step will kill snake.
 
   Serial.println(up);
   Serial.println(right);
@@ -123,6 +120,7 @@ void Snake::findDirection() {
     for (const int &n: this->position) {
       Serial.println(n);
     }
+    end();
   } else if (up > right && up > down && up > left) {
     // go up!
     moveSnake(snakehead - 16);
@@ -144,8 +142,6 @@ void Snake::findDirection() {
 
 void Snake::moveSnake(int newpos) 
 {
-  delay(100);
-
   if (newpos == this->dot) {
     Serial.println("arrived dot!");
     this->position.push_back(newpos);
@@ -160,184 +156,48 @@ void Snake::moveSnake(int newpos)
     this->position.erase(this->position.begin()); // removing first element (end) of snake
     
     Screen.render();
-
-    findDirection();
   }
 
 }
 
-void Snake::newLevel()
-{
-  //this->initBricks();
-  for (byte i = 0; i < Snake::PADDLE_WIDTH; i++)
-  {
-    this->paddle[i].x = (Snake::X_MAX / 2) - (Snake::PADDLE_WIDTH / 2) + i;
-    this->paddle[i].y = Snake::Y_MAX - 1;
-    Screen.setPixelAtIndex(this->paddle[i].y * Snake::X_MAX + paddle[i].x, Snake::LED_TYPE_ON);
-  }
-  this->ball.x = this->paddle[1].x;
-  this->ball.y = this->paddle[1].y - 1;
-
-  Screen.setPixelAtIndex(ball.y * Snake::X_MAX + ball.x, Snake::LED_TYPE_ON);
-  this->ballMovement[0] = 1;
-  this->ballMovement[1] = -1;
-  this->lastBallUpdate = 0;
-
-  Screen.render();
-  this->level++;
-  this->gameState = Snake::GAME_STATE_RUNNING;
-}
-
-void Snake::updateBall()
-{
-  if ((millis() - this->lastBallUpdate) < this->ballDelay)
-  {
-    return;
-  }
-  this->lastBallUpdate = millis();
-  Screen.setPixelAtIndex(this->ball.y * Snake::X_MAX + this->ball.x, Snake::LED_TYPE_OFF);
-
-  if (this->ballMovement[1] == 1)
-  {
-    // collision with bottom
-    if (this->ball.y == (Snake::Y_MAX - 1))
-    {
-      this->end();
-      return;
-    }
-    this->checkPaddleCollision();
-  }
-
-  // collision detection with bricks
-  for (byte i = 0; i < Snake::BRICK_AMOUNT; i++)
-  {
-    if (this->bricks[i].x == this->ball.x && this->bricks[i].y == this->ball.y)
-    {
-      this->hitBrick(i);
-      break;
-    }
-  }
-  if (this->destroyedBricks >= Snake::BRICK_AMOUNT)
-  {
-    this->gameState = Snake::GAME_STATE_LEVEL;
-    return;
-  }
-
-  // collision detection with wall
-  if (this->ball.x <= 0 || this->ball.x >= (Snake::X_MAX - 1))
-  {
-    this->ballMovement[0] *= -1;
-  }
-  if (this->ball.y <= 0)
-  {
-    this->ballMovement[1] *= -1;
-  }
-
-  this->ball.x += this->ballMovement[0];
-  this->ball.y += this->ballMovement[1];
-
-  Screen.setPixelAtIndex(this->ball.y * Snake::X_MAX + this->ball.x, Snake::LED_TYPE_ON);
-  Screen.render();
-}
-
-void Snake::hitBrick(byte i)
-{
-  this->bricks[i].x = -1;
-  this->bricks[i].y = -1;
-  // ballMovement[1] *= -1;
-  this->score++;
-  this->destroyedBricks++;
-  if (this->ballDelay > Snake::BALL_DELAY_MIN)
-  {
-    this->ballDelay -= Snake::BALL_DELAY_STEP;
-  }
-  Screen.setPixelAtIndex(this->bricks[i].y * Snake::X_MAX + this->bricks[i].x, Snake::LED_TYPE_OFF);
-}
-
-void Snake::checkPaddleCollision()
-{
-  if ((this->paddle[0].y - 1) != this->ball.y)
-  {
-    return;
-  }
-
-  // reverse movement direction on the edges
-  if (this->ballMovement[0] == 1 && (this->paddle[0].x - 1) == this->ball.x ||
-      this->ballMovement[0] == -1 && (this->paddle[Snake::PADDLE_WIDTH - 1].x + 1) == this->ball.x)
-  {
-    this->ballMovement[0] *= -1;
-    this->ballMovement[1] *= -1;
-
-    return;
-  }
-  if (paddle[Snake::PADDLE_WIDTH / 2].x == this->ball.x)
-  {
-    this->ballMovement[0] = 0;
-    this->ballMovement[1] *= -1;
-
-    return;
-  }
-
-  for (byte i = 0; i < Snake::PADDLE_WIDTH; i++)
-  {
-    if (this->paddle[i].x == this->ball.x)
-    {
-      this->ballMovement[1] *= -1;
-      if (random(2) == 0)
-      {
-        this->ballMovement[0] = 1;
-      }
-      else
-      {
-        this->ballMovement[0] = -1;
-      }
-
-      break;
-    }
-  }
-}
-
-void Snake::updatePaddle()
-{
-  byte direction = random(0, 2) > 0 ? 1 : 2;
-
-  unsigned int moveDirection = 0;
-  if (direction == Snake::DIRECTION_LEFT && this->paddle[0].x > 0)
-  {
-    moveDirection = -1;
-  }
-  if (direction == Snake::DIRECTION_RIGHT && this->paddle[Snake::PADDLE_WIDTH - 1].x < (Snake::X_MAX - 1))
-  {
-    moveDirection = 1;
-  }
-
-  if (moveDirection != 0)
-  {
-    // turn off paddle LEDs
-    for (byte i = 0; i < Snake::PADDLE_WIDTH; i++)
-    {
-      Screen.setPixelAtIndex(this->paddle[i].y * Snake::X_MAX + this->paddle[i].x, Snake::LED_TYPE_OFF);
-    }
-    for (byte i = 0; i < Snake::PADDLE_WIDTH; i++)
-    {
-      this->paddle[i].x += moveDirection;
-    }
-    for (byte i = 0; i < Snake::PADDLE_WIDTH; i++)
-    {
-      Screen.setPixelAtIndex(this->paddle[i].y * Snake::X_MAX + this->paddle[i].x, Snake::LED_TYPE_ON);
-    }
-  }
-  Screen.render();
-}
 
 void Snake::end()
 {
-  // Serial.println("GAME OVER!");
-  this->gameState = Snake::GAME_STATE_END;
-  Screen.setPixelAtIndex(this->ball.y * Snake::X_MAX + this->ball.x, Snake::LED_TYPE_ON);
+  Serial.println("GAME OVER!");
 
+  for (const int &n: this->position) { Screen.setPixelAtIndex(n, Snake::LED_TYPE_OFF); }
   Screen.render();
-  // Serial.println("Final score: " + String(score) + " in level " + String(level));
+  delay(200);
+
+  for (const int &n: this->position) { Screen.setPixelAtIndex(n, Snake::LED_TYPE_ON); }
+  Screen.render();
+  delay(200);
+
+  for (const int &n: this->position) { Screen.setPixelAtIndex(n, Snake::LED_TYPE_OFF); }
+  Screen.render();
+  delay(200);
+
+  for (const int &n: this->position) { Screen.setPixelAtIndex(n, Snake::LED_TYPE_ON); }
+  Screen.render();
+  delay(200);
+
+  for (const int &n: this->position) { Screen.setPixelAtIndex(n, Snake::LED_TYPE_OFF); }
+  Screen.render();
+  delay(200);
+
+  for (const int &n: this->position) { Screen.setPixelAtIndex(n, Snake::LED_TYPE_ON); }
+  Screen.render();
+  delay(500);
+
+  for (const int &n: this->position) { Screen.setPixelAtIndex(n, Snake::LED_TYPE_OFF); Screen.render(); delay(200); }
+  
+  delay(200);
+  Screen.setPixelAtIndex(this->dot, Snake::LED_TYPE_OFF);
+  Screen.render();
+  delay(500);
+
+  this->gameState = Snake::GAME_STATE_END;
+
 }
 
 void Snake::setup()
@@ -350,14 +210,9 @@ void Snake::loop()
   listenOnButtonToChangeMode();
   switch (this->gameState)
   {
-  case Snake::GAME_STATE_LEVEL:
-    this->newLevel();
-    break;
   case Snake::GAME_STATE_RUNNING:
-    //this->moveSnake();
-    //this->updateBall();
-    //this->updatePaddle();
-    //delay(300);
+    this->findDirection();
+    delay(100);
     break;
   case Snake::GAME_STATE_END:
     this->initGame();
