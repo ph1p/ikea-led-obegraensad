31b7bb9271dbc7b17473064f771aae307e2edfd3
Ricky Palaguachi <rickyp2413@gmail.com>
2023-01-11 16:50:34 -0500

    Added mode `BigClock` with bigger, bolder numbers.
    * Added new `bigNumbers` symbols to `sign.cpp` with bigger, bolder
      numerals.
    * Added new function `displayBigNumbers`
    * Added a new `class BigClock` that handles displaying the clock.
    * Added a utility script for handy binary-to-hex conversions.
    * IMPORTANT: Removed hardcoded values when the `MODE` enum is safer.
    * Rebuilt the webgui.cpp

---
+++ util/bin2hex.py
@@ -0,0 +1,10 @@
+# Utility script that converts binary to a C++ hex array and prints to stdout.
+# Each line of binary corresponds to an element in the output array.
+
+with open("bits", "r") as f:
+    nums = []
+    for line in f.readlines():
+        num = int(line, 2)
+        hex_num = hex(num)
+        nums.append(hex_num)
+    print("{" + ", ".join(nums) + "}")
