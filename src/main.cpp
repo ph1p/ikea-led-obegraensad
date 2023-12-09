3562af26290c01bc628c45fb4e60b327c788a5a9
Andreas Huber <ahuber1121@gmail.com>
2023-12-09 11:12:50 +0100

    revert src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,37 @@
 #include <Arduino.h>
 #include <SPI.h>
 
-#ifdef ESP32
-#include <WiFiManager.h>
-#endif
-#ifdef ESP32
-#include <ESPmDNS.h>
-#endif
 #ifdef ESP8266
 #include <ESP8266WiFi.h>
+#endif
+#ifdef ESP32
+#include <WiFi.h>
 #endif
 
 #include "PluginManager.h"
 
-#include "plugins/BreakoutPlugin.h"
-#include "plugins/CirclePlugin.h"
 #include "plugins/DrawPlugin.h"
-#include "plugins/FireworkPlugin.h"
-#include "plugins/GameOfLifePlugin.h"
-#include "plugins/LinesPlugin.h"
-#include "plugins/RainPlugin.h"
+#include "plugins/BreakoutPlugin.h"
 #include "plugins/SnakePlugin.h"
+#include "plugins/GameOfLifePlugin.h"
 #include "plugins/StarsPlugin.h"
+#include "plugins/LinesPlugin.h"
+#include "plugins/CirclePlugin.h"
+#include "plugins/RainPlugin.h"
+#include "plugins/FireworkPlugin.h"
 
 #ifdef ENABLE_SERVER
-#include "plugins/AnimationPlugin.h"
 #include "plugins/BigClockPlugin.h"
 #include "plugins/ClockPlugin.h"
 #include "plugins/WeatherPlugin.h"
+#include "plugins/AnimationPlugin.h"
 #endif
 
-#include "asyncwebserver.h"
-#include "ota.h"
-#include "screen.h"
-#include "secrets.h"
 #include "websocket.h"
+#include "secrets.h"
+#include "ota.h"
+#include "webserver.h"
+#include "screen.h"
 
 unsigned long previousMillis = 0;
 unsigned long interval = 30000;
@@ -42,92 +39,44 @@ unsigned long interval = 30000;
 PluginManager pluginManager;
 SYSTEM_STATUS currentStatus = NONE;
 
-#ifdef ESP32
-WiFiManager wifiManager;
-#endif
-
 unsigned long lastConnectionAttempt = 0;
 const unsigned long connectionInterval = 10000;
 
-#ifdef ESP32
-void connectToWiFi() {
-
-  // if a WiFi setup AP was started, reboot is required to clear routes
-  bool wifiWebServerStarted = false;
-  wifiManager.setWebServerCallback(
-      [&wifiWebServerStarted]() { wifiWebServerStarted = true; });
-
-  wifiManager.setHostname(WIFI_HOSTNAME);
-  wifiManager.autoConnect(WIFI_MANAGER_SSID);
-
-  if (MDNS.begin(WIFI_HOSTNAME)) {
-    MDNS.addService("http", "tcp", 80);
-    MDNS.setInstanceName(WIFI_HOSTNAME);
-  } else {
-    Serial.println("Could not start mDNS!");
-  }
-
-  if (wifiWebServerStarted) {
-    // Reboot required, otherwise wifiManager server interferes with our server
-    Serial.println("Done running WiFi Manager webserver - rebooting");
-    ESP.restart();
-  }
-
-  lastConnectionAttempt = millis();
-}
-#endif
-
-#ifdef ESP8266
-void connectToWiFi() {
+void connectToWiFi()
+{
   Serial.println("Connecting to Wi-Fi...");
 
   // Delete old config
   WiFi.disconnect(true);
-
-#if defined(IP_ADDRESS) && defined(GWY) && defined(SUBNET) && defined(DNS1) && \
-    defined(DNS2)
-  auto ip = IPAddress();
-  ip.fromString(IP_ADDRESS);
-
-  auto gwy = IPAddress();
-  gwy.fromString(GWY);
-
-  auto subnet = IPAddress();
-  subnet.fromString(SUBNET);
-
-  auto dns1 = IPAddress();
-  dns1.fromString(DNS1);
-
-  auto dns2 = IPAddress();
-  dns2.fromString(DNS2);
-
-  WiFi.config(ip, gwy, subnet, dns1, dns2);
-#endif
 
   WiFi.setHostname(WIFI_HOSTNAME);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
   // Wait for connection
   int attempts = 0;
-  while (WiFi.status() != WL_CONNECTED && attempts < 20) {
+  while (WiFi.status() != WL_CONNECTED && attempts < 20)
+  {
     delay(500);
     Serial.print(".");
     attempts++;
   }
 
   // Check connection result
-  if (WiFi.status() == WL_CONNECTED) {
+  if (WiFi.status() == WL_CONNECTED)
+  {
     Serial.print("Connected to WiFi network with IP Address: ");
     Serial.println(WiFi.localIP());
-  } else {
+  }
+  else
+  {
     Serial.println("\nFailed to connect to Wi-Fi. Please check credentials.");
   }
 
   lastConnectionAttempt = millis();
 }
-#endif
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
 
   pinMode(PIN_LATCH, OUTPUT);
@@ -151,30 +100,31 @@ void setup() {
   Screen.setup();
 
   pluginManager.addPlugin(new DrawPlugin());
-  // pluginManager.addPlugin(new BreakoutPlugin());
+  pluginManager.addPlugin(new BreakoutPlugin());
   pluginManager.addPlugin(new SnakePlugin());
-  // pluginManager.addPlugin(new GameOfLifePlugin());
+  pluginManager.addPlugin(new GameOfLifePlugin());
   pluginManager.addPlugin(new StarsPlugin());
-  // pluginManager.addPlugin(new LinesPlugin());
-  // pluginManager.addPlugin(new CirclePlugin());
+  pluginManager.addPlugin(new LinesPlugin());
+  pluginManager.addPlugin(new CirclePlugin());
   pluginManager.addPlugin(new RainPlugin());
-  // pluginManager.addPlugin(new FireworkPlugin());
+  pluginManager.addPlugin(new FireworkPlugin());
 
 #ifdef ENABLE_SERVER
   pluginManager.addPlugin(new BigClockPlugin());
   pluginManager.addPlugin(new ClockPlugin());
   pluginManager.addPlugin(new WeatherPlugin());
-  // pluginManager.addPlugin(new AnimationPlugin());
+  pluginManager.addPlugin(new AnimationPlugin());
 #endif
 
   pluginManager.init();
 }
 
-void loop() {
+void loop()
+{
   pluginManager.runActivePlugin();
 
-  if (WiFi.status() != WL_CONNECTED &&
-      millis() - lastConnectionAttempt > connectionInterval) {
+  if (WiFi.status() != WL_CONNECTED && millis() - lastConnectionAttempt > connectionInterval)
+  {
     Serial.println("Lost connection to Wi-Fi. Reconnecting...");
     connectToWiFi();
   }
