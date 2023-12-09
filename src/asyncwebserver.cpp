5125748f17693ef5d6db94fed5b962713756c0d8
Andreas Huber <ahuber1121@gmail.com>
2023-12-09 11:20:47 +0100

    refactor: undo auto-format

--- src/asyncwebserver.cpp
+++ src/asyncwebserver.cpp
@@ -4,17 +4,15 @@
 
 AsyncWebServer server(80);
 
-void initWebServer() {
-  DefaultHeaders::Instance().addHeader("Access-Control-Allow-Headers",
-                                       "Accept, Content-Type, Authorization");
-  DefaultHeaders::Instance().addHeader("Access-Control-Allow-Credentials",
-                                       "true");
+void initWebServer()
+{
+  DefaultHeaders::Instance().addHeader("Access-Control-Allow-Headers", "Accept, Content-Type, Authorization");
+  DefaultHeaders::Instance().addHeader("Access-Control-Allow-Credentials", "true");
   DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
 
   server.on("/", HTTP_GET, startGui);
-  server.onNotFound([](AsyncWebServerRequest *request) {
-    request->send(404, "text/plain", "Page not found!");
-  });
+  server.onNotFound([](AsyncWebServerRequest *request)
+                    { request->send(404, "text/plain", "Page not found!"); });
 
   server.begin();
 }
