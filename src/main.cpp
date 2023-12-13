a875e5cdeb9cc76f498b6aecf333e1235b065577
Andreas Huber <ahuber1121@gmail.com>
2023-12-13 09:01:02 +0100

    fix: compile errors when using WiFiManager.h on ESP8266

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
 #include <SPI.h>
 
-#ifdef ESP32
-#include <WiFiManager.h>
+#ifdef ESP82666
+/* Fix duplicate defs of HTTP_GET, HTTP_POST, ... in ESPAsyncWebServer.h */
+#define WEBSERVER_H
 #endif
+#include <WiFiManager.h>
+
 #ifdef ESP32
 #include <ESPmDNS.h>
 #endif
@@ -41,38 +44,30 @@ unsigned long interval = 30000;
 
 PluginManager pluginManager;
 SYSTEM_STATUS currentStatus = NONE;
-#ifdef ESP32
 WiFiManager wifiManager;
-#endif
 
 unsigned long lastConnectionAttempt = 0;
 const unsigned long connectionInterval = 10000;
 
-#ifdef ESP32
-void connectToWiFi()
-{
-
+void connectToWiFi() {
   // if a WiFi setup AP was started, reboot is required to clear routes
   bool wifiWebServerStarted = false;
   wifiManager.setWebServerCallback(
-      [&wifiWebServerStarted]()
-      { wifiWebServerStarted = true; });
+      [&wifiWebServerStarted]() { wifiWebServerStarted = true; });
 
   wifiManager.setHostname(WIFI_HOSTNAME);
   wifiManager.autoConnect(WIFI_MANAGER_SSID);
 
-  if (MDNS.begin(WIFI_HOSTNAME))
-  {
+  #ifdef ESP32
+  if (MDNS.begin(WIFI_HOSTNAME)) {
     MDNS.addService("http", "tcp", 80);
     MDNS.setInstanceName(WIFI_HOSTNAME);
-  }
-  else
-  {
+  } else {
     Serial.println("Could not start mDNS!");
   }
+  #endif
 
-  if (wifiWebServerStarted)
-  {
+  if (wifiWebServerStarted) {
     // Reboot required, otherwise wifiManager server interferes with our server
     Serial.println("Done running WiFi Manager webserver - rebooting");
     ESP.restart();
@@ -80,65 +75,8 @@ void connectToWiFi()
 
   lastConnectionAttempt = millis();
 }
-#endif
 
-#ifdef ESP8266
-void connectToWiFi()
-{
-  Serial.println("Connecting to Wi-Fi...");
-
-  // Delete old config
-  WiFi.disconnect(true);
-
-#if defined(IP_ADDRESS) && defined(GWY) && defined(SUBNET) && defined(DNS1) && \
-    defined(DNS2)
-  auto ip = IPAddress();
-  ip.fromString(IP_ADDRESS);
-
-  auto gwy = IPAddress();
-  gwy.fromString(GWY);
-
-  auto subnet = IPAddress();
-  subnet.fromString(SUBNET);
-
-  auto dns1 = IPAddress();
-  dns1.fromString(DNS1);
-
-  auto dns2 = IPAddress();
-  dns2.fromString(DNS2);
-
-  WiFi.config(ip, gwy, subnet, dns1, dns2);
-#endif
-
-  WiFi.setHostname(WIFI_HOSTNAME);
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-
-  // Wait for connection
-  int attempts = 0;
-  while (WiFi.status() != WL_CONNECTED && attempts < 20)
-  {
-    delay(500);
-    Serial.print(".");
-    attempts++;
-  }
-
-  // Check connection result
-  if (WiFi.status() == WL_CONNECTED)
-  {
-    Serial.print("Connected to WiFi network with IP Address: ");
-    Serial.println(WiFi.localIP());
-  }
-  else
-  {
-    Serial.println("\nFailed to connect to Wi-Fi. Please check credentials.");
-  }
-
-  lastConnectionAttempt = millis();
-}
-#endif
-
-void setup()
-{
+void setup() {
   Serial.begin(115200);
 
   pinMode(PIN_LATCH, OUTPUT);
