9bb2575648ebbadf06381f43b1cfcd8ea2819f4a
gelse <49122643+gelse@users.noreply.github.com>
2024-04-05 17:07:16 +0200

    feat(api): add endpoints and improve api
    #100 
    
    ---------
    
    Co-authored-by: Michael Kohl <michael.kohl@sap.com>
    Co-authored-by: Michael <michael@kohlsalem.com>

---
+++ include/webhandler.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "ESPAsyncWebServer.h"
+
+void handleMessage(AsyncWebServerRequest *request);
+void handleRemove(AsyncWebServerRequest *request);
+void handleGetStatus(AsyncWebServerRequest *request);
+void handleGetMetadata(AsyncWebServerRequest *request);
+void handleSetPlugin(AsyncWebServerRequest *request);
+void handleSetBrightness(AsyncWebServerRequest *request);
+void handleGetData(AsyncWebServerRequest *request);
--- src/asyncwebserver.cpp
+++ src/asyncwebserver.cpp
@@ -1,5 +1,6 @@
 #include "asyncwebserver.h"
 #include "messages.h"
+#include "webhandler.h"
 
 #ifdef ENABLE_SERVER
 
@@ -18,6 +19,20 @@ void initWebServer()
   // Route to handle  http://your-server/message?text=Hello&repeat=3&id=42&delay=30&graph=1,2,3,4&miny=0&maxy=15
   server.on("/message", HTTP_GET, handleMessage);
   server.on("/removemessage", HTTP_GET, handleRemove);
+  
+  // Handle API request to get status
+  server.on("/getStatus", HTTP_GET, handleGetStatus);
+
+  // Handle API request to get status
+  server.on("/getMetadata", HTTP_GET, handleGetMetadata);
+
+  // Handle API request to set an active plugin by ID
+  server.on("/setplugin", HTTP_PATCH, handleSetPlugin);
+
+  // Handle API request to set the brightness (0..255);
+  server.on("/setbrightness", HTTP_PATCH, handleSetBrightness);
+  server.on("/getData", HTTP_GET, handleGetData);
+
   server.begin();
 }
 
