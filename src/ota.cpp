cb9b0bb4d47f9dd3cbebd2522ae103ffc9fa4f78
Solmath <thommy86@gmail.com>
2025-06-08 17:49:52 +0200

    Update OTA handling and dependencies
    - Changed the ESPAsyncWebServer library dependency to the correct repository.
    - Added ElegantOTA library for improved OTA functionality.
    - Enhanced OTA callback functions to include logging for start, progress, and end of OTA updates.
    - Updated the initOTA function to use the new ElegantOTA library and its callbacks.

--- src/ota.cpp
+++ src/ota.cpp
@@ -5,37 +5,46 @@
 const char *otaUser = OTA_USERNAME;
 const char *otaPassword = OTA_PASSWORD;
 
+unsigned long ota_progress_millis = 0;
+
 void onOTAStart()
 {
-  currentStatus = UPDATE;
-
-  std::vector<int> bits = Screen.readBytes(letterU);
-
-  for (int i = 0; i < bits.size(); i++)
-  {
-    Screen.setPixelAtIndex(i, bits[i]);
-  }
+    // Log when OTA has started
+    Serial.println("OTA update started!");
+    // <Add your own code here>
 }
 
-void onOTAEnd()
+void onOTAProgress(size_t current, size_t final)
 {
-  std::vector<int> bits = Screen.readBytes(letterR);
+    // Log every 1 second
+    if (millis() - ota_progress_millis > 1000)
+    {
+        ota_progress_millis = millis();
+        Serial.printf("OTA Progress Current: %u bytes, Final: %u bytes\n", current, final);
+    }
+}
 
-  for (int i = 0; i < bits.size(); i++)
-  {
-    Screen.setPixelAtIndex(i, bits[i]);
-  }
-
-  delay(1000);
-  currentStatus = NONE;
-  Screen.loadFromStorage();
+void onOTAEnd(bool success)
+{
+    // Log when OTA has finished
+    if (success)
+    {
+        Serial.println("OTA update finished successfully!");
+    }
+    else
+    {
+        Serial.println("There was an error during OTA update!");
+    }
+    // <Add your own code here>
 }
 
 void initOTA(AsyncWebServer &server)
 {
-  AsyncElegantOTA.begin(&server, otaUser, otaPassword);
-  AsyncElegantOTA.onOTAStart(onOTAStart);
-  AsyncElegantOTA.onOTAEnd(onOTAEnd);
+    ElegantOTA.begin(&server); // Start ElegantOTA
+    // ElegantOTA callbacks
+    ElegantOTA.onStart(onOTAStart);
+    ElegantOTA.onProgress(onOTAProgress);
+    ElegantOTA.onEnd(onOTAEnd);
 }
 
 #endif
