64b1280755ed148aa74fcb9473a42249aae042bb
Joel Rupp <joel.rupp05@gmail.com>
2025-06-12 23:07:48 +0200

    Fixed defines

--- extra_script.py
+++ extra_script.py
@@ -5,11 +5,16 @@ username = env.GetProjectOption("custom_username")
 password = env.GetProjectOption("custom_password")
 ssid = env.GetProjectOption("custom_wifi_ssid")
 
+print("Username:", username)
+print("Password:", password)
+print("SSID:", ssid)
+
+
 # Als Build-Flags für C++ hinzufügen
 env.Append(
     CPPDEFINES=[
-        ("DOTA_USERNAME", '"%s"' % username),
-        ("DOTA_PASSWORD", '"%s"' % password),
-        ("WIFI_SSID", '"%s"' % ssid)
+        ("OTA_USERNAME", '\\"%s\\"' % username),
+        ("OTA_PASSWORD", '\\"%s\\"' % password),
+        ("WIFI_HOSTNAME", '\\"%s\\"' % ssid)
     ]
 )
